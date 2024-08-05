e56d9897fd2fbbf420d018439fa3f864573997ed
bragadeeshwaran06 <bragadeeshwaran@testpress.in>
2024-08-05 15:29:26 +0530

    Update urls

--- classifides/ads/urls.py
+++ classifides/ads/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from ads.views import *
 from ads import views
+
 urlpatterns = [
     path('',views.home,name='home'),
-    path('category/<int:category_id>/', category_ads, name='category_ads'),
 ]
 
