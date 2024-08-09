236365a02376e1ba2cf12ef9452b83cc1f4fb3ff
bragadeeshwaran06 <bragadeeshwaran@testpress.in>
2024-08-09 11:36:56 +0530

    Update models.py

--- classifides/ads/models.py
+++ classifides/ads/models.py
@@ -26,7 +26,6 @@ class Category(models.Model):
 class Ad(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='ads/images/', blank=True, null=True)
     tags = TaggableManager()
     location = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
@@ -35,13 +34,21 @@ class Ad(models.Model):
     contact_phone = models.CharField(max_length=20, blank=True, null=True)
     show_contact_info = models.BooleanField(default=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    event_dates = models.DateField(blank=True, null=True)  # For events
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class AdImage(models.Model):
+    image = models.ImageField(upload_to='ad_images/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, related_name='images', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Image for {self.ad.title}'
+
     
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
