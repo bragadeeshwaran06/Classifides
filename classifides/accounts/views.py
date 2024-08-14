bea290426eccbd96c01ed4f5499ac35028fdb5c6
Bragadeeshwaran <bragadeeshwaran@testpress.in>
2024-08-14 15:57:12 +0530

    Update  accounts/views.py

--- classifides/accounts/views.py
+++ classifides/accounts/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from ads.models import Ad  
 from .forms import CustomUserCreationForm
 from django.contrib.auth import logout
+from django.views.decorators.http import require_http_methods
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data.get('email')
-            if User.objects.filter(email=email).exists():
-                form.add_error('email', 'Email is already in use')
-            else:
-                user = form.save()  
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password1')
-                user = authenticate(username=username, password=password)  
-                login(request, user) 
-                return redirect('home') 
+            user = form.save()  
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)  
+            login(request, user) 
+            return redirect('home') 
     else:
         form = CustomUserCreationForm()
 
     return render(request, 'registration/register.html', {'form': form})
-
 
 @login_required
 def user_profile(request):
@@ -32,10 +27,8 @@ def user_profile(request):
     ads = Ad.objects.filter(user=user)  
     return render(request, 'user_profile.html', {'user': user, 'ads': ads})
 
+@require_http_methods(["GET", "POST"])
 def custom_logout_view(request):
-    if request.method == 'POST' or request.method == 'GET':
-        logout(request)
-        referer = request.META.get('HTTP_REFERER', '/')
-        return redirect(referer)
-    else:
-        return redirect('/')
+    logout(request)
+    referer = request.META.get('HTTP_REFERER', '/')
+    return redirect(referer)
