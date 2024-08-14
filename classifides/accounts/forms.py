5fa2c8237dccbfb7915fd949adcee4151b5b3bc0
Bragadeeshwaran <bragadeeshwaran@testpress.in>
2024-08-14 13:13:42 +0530

    Add test.py

--- classifides/accounts/forms.py
+++ classifides/accounts/forms.py
@@ -9,9 +9,9 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
-    def save(self, commit=True):
-        user = super(CustomUserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email is already in use')
+        return email
+
